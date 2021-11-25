84e4e196d4a7ac42d3eb1c82a71ba6f1b9a9c6ce
petulienocka <petra.piknova8@gmail.com>
2021-11-25 16:00:54 +0100

    make it better

--- routes/users.js
+++ routes/users.js
@@ -61,4 +61,14 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+// Get all
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
